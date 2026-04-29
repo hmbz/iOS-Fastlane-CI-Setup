8783e37a6592f4b1b8800542b494e833827fd872
Bilal-zafar-ios <bilal.zafar@ssasoft.com>
2026-04-29 18:26:37 +0500

    Add ci_post_clone script for auto build number increment

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Xcode Cloud automatically is script ko run karta hai
+# Har build pe build number auto-increment hota hai
+
+set -e
+
+# CI_BUILD_NUMBER Xcode Cloud ka built-in variable hai
+# Yeh har build pe automatically increase hota hai
+echo "Setting build number to: $CI_BUILD_NUMBER"
+
+cd $CI_WORKSPACE
+agvtool new-version -all $CI_BUILD_NUMBER
+
+echo "✅ Build number set to $CI_BUILD_NUMBER"
